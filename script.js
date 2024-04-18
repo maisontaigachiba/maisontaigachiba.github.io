7c4aef9703fd3aadec8679d036513cae1d92cb8b
maisontaigachiba <timsofy1@gmail.com>
2024-04-18 18:48:46 +0900

    add contents

--- script.js
+++ script.js
@@ -15,17 +15,111 @@ var closing = {
     title: "クロージング",
     content: "4/15の14:00よりお時間をいただきます。追ってメールをお送りいたします。"
 }
+var whatService = {
+    id: "whatService",
+    title: "どんなサービス？",
+    content: "弊社クラウドテックでは、他社と比べても母数の大きい600万のデータベースからフリーランスの方をご提案可能です。<br>またエージェントとしての知見もありますので、市況感や、他社さんがどのように活用されているか等もお伝え出来ます。"
+}
+var busyNow = {
+    id: "busyNow",
+    title: "忙しいのでまた今度",
+    content: "ありがとうございます、O月とかって少し落ち着いていますでしょうか？<br>ではまたO月に改めさせていただきます。<br>本日サービス資料だけメールで送付させていただいてもよろしいでしょうか？<br>(アドレス・担当名確認)"
+}
+var busyNowAnoter = {
+    id: "busyNowAnoter",
+    title: "忙しいのでまた今度(別バージョン)",
+    content: "月末忙しいですよね〜、すみません！来月の初めとかっていかがでしょうか？<br>※前提上記を挟ませない様、一息で言い切る<br>∟また「話してて取れそうだけど取れない」状況では「お電話対応の御礼メール→調整」<br>に持ち込むのも有効"
+}
+var notNow = {
+    id: "notNow",
+    title: "すぐには困っていない",
+    content: "ありがとうございます。今回ご連絡差し上げたのは、すぐに使っていただきたい訳ではなく<br>現在の採用手法との比較検討材料としてサービスのご紹介をさせていただきたいのですが…"
+}
+var notNowAnother = {
+    id: "notNowAnother",
+    title: "すぐには困っていない(別バージョン)",
+    content: "ありがとうございます。<br>他社様でも求人前にどういったスキルセットを持った人材がいるのか、その費用感までご参考にして頂くケースも多かったので、是非一度こちらのご紹介も含めて…(日時打診)"
+}
+var usingAnotherService = {
+    id: "usingAnotherService",
+    title: "既に他社を使っている",
+    content: "すぐに使っていただきたいわけではなくて、現在の採用手法との比較検討材料として<br>是非サービスのご紹介とご挨拶させていただきたいなと思っているのですが<br>今週とか来週で30分ほどオンラインでお時間頂くことは可能でしょうか？"
+}
+var aboutContract = {
+    id: "aboutContract",
+    title: "契約面について",
+    content: "弊社業務委託でのご紹介になります。<br>準委任契約でご登録していただいている個人事業主様をご紹介という形になります。"
+}
+var recruitmentWell = {
+    id: "recruitmentWell",
+    title: "採用は順調に進んでいる",
+    content: "順調でいらっしゃるんですね！<br>クラウドテックは最短3日で稼働開始の実績もあるので、<br>不測の事態に備えて、一度サービスをご説明するお時間頂ければと思うのですが(日時打診へ)"
+}
+var notRecruitment = {
+    id: "notRecruitment",
+    title: "採用の予定はない",
+    content: "ありがとうございます、今後募集を再開される時期とか、よく募集される時期ってありますか？<br>→是非御社の今後の採用計画に当社のサービスも加えていただきたく<br>→度サービスのご紹介とご挨拶を今週とか来週で30分ほどオンラインで<br>お時間頂くことは可能でしょうか？"
+}
+var onlyFullTime = {
+    id: "onlyFullTime",
+    title: "正社員しか無理",
+    content: "ありがとうございます、それで申しますと弊社登録の人材は正社員への切り替え雇用も可能になっておりまして<br>実際に登録の3割以上の方が正社員への登用を希望されております。<br>他社様でも試用期間としてフリーランス、その後登用と言った事例は多くございますので<br>是非その点含めたサービスのご紹介とご挨拶を今週とか来週で30分ほどオンラインでお時間頂くことは可能でしょうか？"
+}
+var notTrustPerformance = {
+    id: "notTrustPerformance",
+    title: "パフォーマンスが信頼できない",
+    content: "御社とフリーランスの方で秘密保持契約も結ばせていただくので、パフォーマンスについては正社員の方と差異はないと思います。<br>また当社のサポートも万全ですので、是非一度その点含めたサービスのご紹介とご挨拶を今週とか来週で30分ほどオンラインでお時間頂くことは可能でしょうか？"
+}
+var wantingNiche = {
+    id: "wantingNiche",
+    title: "OOの人材が欲しい(ニッチな需要の求人の場合)",
+    content: "・ありがとうございます。弊社人材DBの中にはそういった人材も登録されている<br>可能性はございます。ただ勤務体系の部分で、弊社基本フルリモートを希望されるワーカーさんが多かったのですがこの辺りはいかがでしょうか？<br><br>※本来週5での稼働も厳しいが、上記ケースだとまずは勤務体系確認が優先"
+}
+var howMuch = {
+    id: "howMuch",
+    title: "「幾らなの？」<br>※聞かれた時のみ",
+    content: "ありがとうございます、それで申し上げますと月間どれくらいで稼働して貰いたい等ご希望等はございますでしょうか？<br>※1人月20万円からが判断基準になる"
+}
+var costNG = {
+    id: "costNG",
+    title: "費用的にNG",
+    content: "①御社の予算ってお伺いしてもよろしいでしょうか？<br>→ありがとうございます、予算に合わせた方ご紹介可能なので是非一度サービスのご紹介とご挨拶を、今週とか来週で30分ほどオンラインでお時間頂くことは可能でしょうか？<br><br>②ありがとうございます。<br>結論登録頂いているワーカーさんの経験に応じて金額は変わって参りますので、当日は御社のニーズに紐づく人材が幾らかをお伝えさせて頂きます。<br><br>※手数料質問も来る。その場合【手数料％は伝えてはいけない】<br>公開してないんですが大体他社様3割前後。"
+}
+var reasonCost = {
+    id: "reasonCost",
+    title: "なぜその費用で紹介可能なのか",
+    content: "稼働時間の調整で可能になってきます。<br>例えば、週5の50万の人材を週2～3で25万も可能になっております。<br>是非一度サービスのご紹介とご挨拶を今週とか来週で30分ほどオンラインで<br>お時間頂くことは可能でしょうか？"
+}
+var howMuchFee = {
+    id: "howMuchFee",
+    title: "紹介料金は幾ら？",
+    content: "紹介料金いくらご紹介自体は料金はかからず、ご参画・ご稼働のタイミングで<br>請求が発生する形となります。<br>派遣のような形をイメージしていただけると良いかと思います。"
+}
+var payingSite = {
+    id: "payingSite",
+    title: "支払いサイクルはどうなってる？",
+    content: "ありがとうございます。<br>クラウドテックとしては基本30日サイトで提供させて頂いております。<br>(月末に支払い頂く形)<br>但し弊社グループの人材サービスも複数展開しておりますので、そちらであれば変更可能でございます。<br>（直接契約できるテックダイレクトとかワーカーと相談し変更可能）"
+}
 
 // 表示する要素の配列
 var elements = {
-    "greeting": greeting,
-    "dateFix": dateFix,
-    "closing": closing,
-
-    // "担当変更",
-    // "ヒアリング",
-
-    // "質問"
+    "whatService": whatService,
+    "busyNow": busyNow,
+    "busyNowAnoter": busyNowAnoter,
+    "notNow": notNow,
+    "notNowAnother": notNowAnother,
+    "usingAnotherService": usingAnotherService,
+    "aboutContract": aboutContract,
+    "recruitmentWell": recruitmentWell,
+    "notRecruitment": notRecruitment,
+    "onlyFullTime": onlyFullTime,
+    "notTrustPerformance": notTrustPerformance,
+    "wantingNiche": wantingNiche,
+    "howMuch": howMuch,
+    "costNG": costNG,
+    "reasonCost": reasonCost,
+    "howMuchFee": howMuchFee,
+    "payingSite": payingSite,
 };
 
 function expandElement(arg) {
